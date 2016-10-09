1ff233aa5115ac42e3d6f4285964577773f4d11a
Moises Linares <blade_corp@hotmail.com>
2016-10-08 21:04:06 -0500

    ok

--- estimuladorapp/src/main/java/com/sysdt/estimuladorapp/controller/MedicoController.java
+++ estimuladorapp/src/main/java/com/sysdt/estimuladorapp/controller/MedicoController.java
@@ -4,26 +4,15 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.tomcat.util.net.SecureNioChannel.ApplicationBufferHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.condition.ConsumesRequestCondition;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sysdt.estimuladorapp.dto.MedicoDTO;
-import com.sysdt.estimuladorapp.dto.ObjetoResponse;
-import com.sysdt.estimuladorapp.dto.UsuarioDTO;
-import com.sysdt.estimuladorapp.enums.RespuestaWS;
 import com.sysdt.estimuladorapp.model.Medico;
 import com.sysdt.estimuladorapp.service.interfaces.MedicoService;
 import com.sysdt.estimuladorapp.service.interfaces.UsuarioService;
-import com.sysdt.estimuladorapp.util.MensajesUtil;
 
 @RestController
 @RequestMapping("/medico")
